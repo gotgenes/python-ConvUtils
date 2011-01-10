a46bcb20d83af86591d9ca200e3de46ed06882eb
lasher <lasher@1fbec0df-f6b4-4bca-857c-de90862b47bd>
2011-01-10 21:20:46 +0000

    Added package classifiers.
    git-svn-id: svn+ssh://bioinformatics.cs.vt.edu/home/murali/svnrepo/src/python/ConvUtils@3437 1fbec0df-f6b4-4bca-857c-de90862b47bd

--- setup.py
+++ setup.py
@@ -8,7 +8,15 @@ setup(
     packages=['convutils', 'convutils.test'],
     url='http://pypi.python.org/pypi/ConvUtils/',
     license='LICENSE.txt',
-    description=("A collection of convenient utilities and pure "
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries'
+    ]
+    description=("A library of convenient utility functions and pure "
         "Python data structures"),
     long_description=open('README.txt').read(),
 )
