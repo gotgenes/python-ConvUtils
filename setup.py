36f938fadda81698db7c6a300b70fc8b435f17e7
Chris Lasher <chris.lasher@gmail.com>
2012-03-23 16:29:05 -0400

    Update setup.py version and Python versions.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='ConvUtils',
-    version='1.0.1',
+    version='1.1',
     author='Christopher D. Lasher',
     author_email='chris.lasher@gmail.com',
     packages=['convutils', 'convutils.test'],
@@ -16,7 +16,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Libraries'
     ],
     description=("A library of convenient utility functions and pure "
